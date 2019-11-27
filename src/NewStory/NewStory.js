fc4063187358fcf1f2718011e93a784c7116a8c2
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-11-27 11:05:40 -0600

    save story fx

--- src/NewStory/NewStory.js
+++ src/NewStory/NewStory.js
@@ -1,28 +1,55 @@
 import React from 'react';
 import config from '../config';
+import ApiContext from '../ApiContext'
 
 import './NewStory.css';
 
-function saveStory(content) {
-	const newStory = {
-		
+export default class NewStory extends React.Component {
+	static defaultProps = {
+		content: ''
 	}
-	fetch(config.API_ENDPOINT + `/stories`, {
-		method: 'POST',
-		headers: {
-			'content-type': 'application/json',
-			'Authorization': `Bearer ${config.API_KEY}`
-		},
-		body: JSON.stringify(newStory)
-	})
-}
+	static contextType = ApiContext
 
-export default function NewStory(props) {
-	return (
-		<div>
-			<p>{props.content}</p>
-			<button className='btnSave' onCLick={() => saveStory(props.content)}>Save Story</button>
-			<p>- - - - -</p>
-		</div>
-	)
+	state = {
+		saved: false,
+		error: null
+	}
+
+	saveStory(content) {
+		const newStory = {
+			content,
+			author: this.context.userId
+		}
+		fetch(config.API_ENDPOINT + `/stories`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+				'Authorization': `Bearer ${config.API_KEY}`
+			},
+			body: JSON.stringify(newStory)
+		})
+			.then(res => {
+				if (!res.ok) {
+					throw new Error(res.status)
+				}
+				return res.json()
+			})
+			.then(() => this.setState({ saved: true }))
+			.catch(error => this.setState({ error }))
+	}
+
+	render() {
+		return (
+			<div>
+				<p>{this.props.content}</p>
+				{this.context.username &&
+					<>
+						{!this.state.saved && <button className='btnSave' onClick={() => this.saveStory(this.props.content)}>Save Story</button>}
+						{this.state.saved && <p>Saved!</p>}
+					</>
+				}
+				<p>- - - - -</p>
+			</div>
+		)
+	}
 }
